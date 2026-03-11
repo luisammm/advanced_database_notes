4e9f0d6dd15d9aff86c0caf42e25da2a78807e83
lumerlo <lmerlo@paystand.com>
2026-03-11 16:51:24 -0600

    Remove outdated session notes and SQL challenge solutions; add new session file for February 24, 2026.

--- sql_challenges/challenge-01/solution.sql
+++ sql_challenges/challenge-01/solution.sql
@@ -1,37 +1,15 @@
--- SQL Lesson 6: Multi-table queries with JOINs
+-- SQL Lesson: Aggregate functions
 
--- 1. Find the domestic and international sales for each movie
-SELECT movies.title, boxoffice.domestic_sales, boxoffice.international_sales
-FROM movies
-JOIN boxoffice
-ON movies.id = boxoffice.movie_id;
-
--- 2. Show the sales numbers for each movie that did better internationally rather than domestically
-SELECT movies.title, boxoffice.domestic_sales, boxoffice.international_sales
-FROM movies
-JOIN boxoffice
-ON movies.id = boxoffice.movie_id
-WHERE boxoffice.international_sales > boxoffice.domestic_sales;
-
--- 3. List all the movies by their ratings in descending order
-SELECT movies.title, boxoffice.rating
-FROM movies
-JOIN boxoffice
-ON movies.id = boxoffice.movie_id
-ORDER BY boxoffice.rating DESC;
-
--- SQL Lesson: Working with multiple tables
-
--- 1. Find the list of all buildings that have employees
-SELECT DISTINCT building
+-- 1. Find the longest time that an employee has been at the studio
+SELECT MAX(years_employed)
 FROM employees;
 
--- 2. Find the list of all buildings and their capacity
-SELECT building_name, capacity
-FROM buildings;
+-- 2. For each role, find the average number of years employed by employees in that role
+SELECT role, AVG(years_employed)
+FROM employees
+GROUP BY role;
 
--- 3. List all buildings and the distinct employee roles in each building (including empty buildings)
-SELECT buildings.building_name, employees.role
-FROM buildings
-LEFT JOIN employees
-ON buildings.building_name = employees.building;
+-- 3. Find the total number of employee years worked in each building
+SELECT building, SUM(years_employed)
+FROM employees
+GROUP BY building;
